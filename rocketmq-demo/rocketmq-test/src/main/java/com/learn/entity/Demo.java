997f51d53fac72a46a994cfd4c862d8cd1d8e7e3
zhangchenchen <540490410@qq.com>
2020-07-12 23:16:45 +0800

    合并调整

--- rocketmq-demo/rocketmq-test/src/main/java/com/learn/entity/Demo.java
+++ rocketmq-demo/rocketmq-test/src/main/java/com/learn/entity/Demo.java
@@ -2,7 +2,6 @@ package com.learn.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
 
 @Data
 @AllArgsConstructor
